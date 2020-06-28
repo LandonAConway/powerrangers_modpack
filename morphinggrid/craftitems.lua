ff35b10a9b26ad1ff38937f0bf6946afcec8247d
LandonAConway <45189291+LandonAConway@users.noreply.github.com>
2020-06-28 14:29:09 -0700

    deletion

--- morphinggrid/craftitems.lua
+++
@@ -1,5 +0,0 @@
-minetest.register_craftitem("morphinggrid:energy", {
-	description = "Morphing Grid Energy",
-	inventory_image = "morphinggrid_energy.png",
-	groups = {not_in_creative_inventory=1},
-})
