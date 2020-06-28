98e06cea13e118ece64f0a710d42900f3408ac5c
LandonAConway <45189291+LandonAConway@users.noreply.github.com>
2020-06-28 14:25:39 -0700

    wrong

--- communicator/init.lua
+++
@@ -1,4 +0,0 @@
-communicator = {}
-
-dofile(minetest.get_modpath("communicator") .. "/communicator.lua")
-dofile(minetest.get_modpath("communicator") .. "/teleportation.lua")
--- communicator/privs.lua
+++
@@ -1,4 +0,0 @@
-minetest.register_privilege("communicator", {
-  description = "Allows players to use communicator chat commands.",
-  give_to_singleplayer = false,
-})
