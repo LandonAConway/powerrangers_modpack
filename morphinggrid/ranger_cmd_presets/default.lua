97a9b556bc2b31534ad80ea4a78e0f3c03210715
LandonAConway <45189291+LandonAConway@users.noreply.github.com>
2022-05-05 06:04:11 -0700

    Update default.lua

--- morphinggrid/ranger_cmd_presets/default.lua
+++ morphinggrid/ranger_cmd_presets/default.lua
@@ -1,3 +1,8 @@
 morphinggrid.ranger_cmd_presets.default = {
-
+    toggle_helmet = {
+        description = "Takes off or puts on a ranger's helmet. The helmet must me wielded to be put on.",
+        func = function(name, text, ranger)
+            
+        end
+    }
 }
